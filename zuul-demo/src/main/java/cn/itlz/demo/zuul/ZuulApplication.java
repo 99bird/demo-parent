22842c8d38b53f251145c27ded3381b54a15b5e7
git <77822013@qq.com>
2018-12-06 01:07:14 +0800

    添加BeanScope的demo

--- zuul-demo/src/main/java/cn/itlz/demo/zuul/ZuulApplication.java
+++ zuul-demo/src/main/java/cn/itlz/demo/zuul/ZuulApplication.java
@@ -2,6 +2,7 @@ package cn.itlz.demo.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableOAuth2Sso
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
--- zuul-demo/src/main/java/cn/itlz/demo/zuul/config/ZuulSecurityConfiguration.java
+++ zuul-demo/src/main/java/cn/itlz/demo/zuul/config/ZuulSecurityConfiguration.java
@@ -1,25 +1,8 @@
 package cn.itlz.demo.zuul.config;
-
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * @author Liuzd QQ: 77822013
- * @since 2018/12/3 0003
- */
 @Configuration
-@EnableOAuth2Sso
-public class ZuulSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class ZuulSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/login","/client/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable();
-    }
+
 }
